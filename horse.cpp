228633f92846bb70d6bb944db72d54e249743e1c
willsaliba <105378722+willsaliba@users.noreply.github.com>
2022-09-19 19:14:54 +0930

    Update horse.cpp

--- horse.cpp
+++ horse.cpp
@@ -3,26 +3,27 @@
 #include "horse.h"
 using namespace std;
 
-horse::horse() {
-  name = ""
-  max_speed = 0;
-  min_speed = 0;
-  level = 1;  
-}
+	horse::horse() {
+      name = "";
+      max_speed = 0;
+      min_speed = 0;
+      level = 1;  
+    }
 
-horse::horse(string _name, double max, double min, int l) {
-  name = _name;
-  max_speed = max;
-  min_speed = min;
-  level = l;  
-}
+	horse::horse(string _name, double max, double min, int l) {
+      name = _name;
+      max_speed = max;
+      min_speed = min;
+      level = l;  
+    }
 
-horse::set_max(double max){ max_speed = max;}
-horse::set_min(double min) { min_speed = min;}
-horse::set_name(string n) { name = n;}
-horse::set_level(int l) { level = l;}
+    void horse::set_max(double max){ max_speed = max;}
+    void horse::set_min(double min) { min_speed = min;}
+    void horse::set_name(string n) { name = n;}
+    void horse::set_level(int l) { level = l;}
 
-horse::get_max() { return max_speed;}
-horse::get_min() { return min_speed;}
-horse::get_name() { return name;}
-horse::get_level() { return level;}
+    double horse::get_max() { return max_speed;}
+    double horse::get_min() { return min_speed;}
+    string horse::get_name() { return name;}
+    int horse::get_level() { return level;}
+
