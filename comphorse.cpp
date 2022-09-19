825c0d1ee0a162210f8ae570641d9ba7bcdcf343
willsaliba <105378722+willsaliba@users.noreply.github.com>
2022-09-19 21:14:29 +0930

    Update comphorse.cpp

--- comphorse.cpp
+++ comphorse.cpp
@@ -1,1 +1,47 @@
+#include <iostream>
+#include <string>
+#include "horse.h"
+#include "comphorse.h"
+using namespace std;
 
+//default constructor of bot horse (probably shouldnt be used)
+comphorse::comphorse() {
+  name = "invalid";
+  race_time = 0;
+  division = 0;    
+}
+
+//constuctor of bot horse
+comphorse::comphorse(int div) {
+  if (div > 0 && div < 4) {    
+    
+    //setting initial name & level of horse
+    name = "tba";
+    division=div;
+    
+    //creates random number between 0.0 and 3.0
+    srand(time(NULL));
+    double rand_n = (rand()%300)/10; rand_n = (rand_n/10);   //creates random number between 0.0 and 3.0
+    
+    //assigning a time based on level
+    if (div==1) {
+      //bots in division 1 will have a random time between 8 and 11 seconds
+      race_time = rand_n + 8;
+
+    } else if (div==2) {
+      //bots in division 2 will have a random time between 10 and 13 seconds
+      race_time = rand_n + 10;
+
+    } else if (div==3) {
+      //bots in division 3 will have a random time between 12 and 15 seconds
+      race_time = rand_n + 12;
+    }
+    
+  } else {
+    //if invalid entry entered characteristics will be set to:
+    name = "invalid";
+    race_time = 0;
+    division = 0;   
+  }
+
+}
