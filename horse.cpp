0b45233af28990091c4fc7e405c366ffcdb6fa15
willsaliba <105378722+willsaliba@users.noreply.github.com>
2022-09-19 22:29:09 +0930

    update

--- horse.cpp
+++ horse.cpp
@@ -5,17 +5,21 @@ using namespace std;
 
 	horse::horse() {
       name = "";
-      division = 1;  
+      division = 3;
+      race_time = 15;
     }
 
-	horse::horse(string _name, int d) {
+	horse::horse(string _name, int div, double time) {
       name = _name;
-      division = d;  
+      division = div;  
+      race_time = time;
     }
-
 
     void horse::set_name(string n) { name = n;}
     string horse::get_name() { return name;}
 
     void horse::set_div(int d) { division = d;}
     int horse::get_div() { return division;}
+
+    double horse::get_racet() {return race_time;}
+
