94a3d612e67a98f9dfe138cc5bb1ce17eeabfbfc
zach <zachshinnic03@outlook.com>
2022-09-22 14:30:56 +0930

    pull

--- horse.cpp
+++ horse.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
   //default constructor (not to be used)
 	horse::horse() {
-      name = "";
+      name = "THIS IS AN ERROR";
       race_time = 15;
     }
 
@@ -16,8 +16,15 @@ using namespace std;
     }
 
   //get and set each attribute
-    void horse::set_name(string n) { name = n;}
-    string horse::get_name() { return name;}
+     void horse::set_name(string n) { 
+      cout<<"name before "<<name<<endl;
+      name = n;
+      cout<<"name after "<<name<<endl;
+}
+      string horse::get_name() {
+        cout<<"name is: "<<name<<endl;
+
+         return name;}
 
     void horse::set_race_t(double t) { race_time = t;}
     double horse::get_race_t() {return race_time;}
